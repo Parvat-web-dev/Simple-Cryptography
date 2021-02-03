5c352a685633cd6b5fb3182efb07c1212ebf77e2
Parvat-web-dev <replyerrors.parvat@gmail.com>
2021-02-03 08:20:50 +0530

    Version: 2.2

--- Cryptography App.py
+++ Cryptography App.py
@@ -2,149 +2,141 @@ import Cryptography as e
 from tkinter import *
 from tkinter import filedialog as f
 from datetime import datetime as d
+from kivy.core.clipboard import Clipboard
 try:
     import webbrowser as wb
 except:
     print('`webbrowser` - package is not installed, so one feature may not work!')
-    
-#The window configuration:
+
 app = Tk()
-app.geometry('700x500')
+app.geometry('600x500')
 app.title('Cryptography')
-app.resizable(False, False)
+app.minsize(350, 500)
+menu = Menu(app)
 
-#The Help Prompt:
-def Credits():
-    c = Tk()
-    c.geometry('510x320')
-    c.resizable(False, False)
-    c.title('Credits')
-    c.config(bg = 'black')
-    txt = '''This GUI App is created by:
-        R. Parvat
-        
-    Launched in github on :
-        Dec 31 2020   12:45
-        
-    This Program is created using:
-        1. Python 3.9   -   Programming Language
-        2. Python IDLE  -   Text Editor
-        3. Tkinter      -   Python Module
-        4. Datetime     -   Python Inbuilt Module
-    '''
-    l = Label(c, text = txt, fg = 'white', bg = 'black', font=(('Curls MT'),(18),('italic')), justify = LEFT)
-    l.place(x = 0, y = 0)
+key = Entry(app, font = (('Courier'), ('20'), ('italic')), justify = CENTER)
+key.place(relx = 0, y = 50, relwidth=0.6)
 
+label1 = Label(app, text = 'Text To Encode/Decode :', font = 'Courier 18')
+label2 = Label(app, text = 'Key :', font = 'Courier 18 bold')
+label1.place(x = 10, y = 90)
+label2.place(x = 10, y = 10)
 
-#The text-input area:
-text_input = Text(app, width = 25, height = 18, font='Courier 18')
-text_input.place(x = 0, y = 50)
+text_input = Text(app, fg ='white', bg = 'navyblue', font = (('Courier'), (18), ('bold')), insertbackground='white')
+text_input.place(relx = 0, y = 150, relwidth=0.5, relheight=0.7)
 
-#The key entry:
-key = Entry(app, bg = 'black', fg = 'white', font='Courier 16')
-key.place(x = 90, y = 12)
-key_l = Label(app, text = 'Key :', font='Arial 16')
-key_l.place(x = 10, y = 10)
+text_return = Label(app, wraplength=323, fg = 'white', bg = 'black', font = 'Consolas 16', text='Your Decode Text Or Encoded Text Will Be Shown Here', justify=LEFT)
+text_return.config(state=NORMAL)
+text_return.place(relx=0.5, y = 150, relheight=0.7, relwidth=0.5)
 
-#the decoded code:
-code = ''
+def go_to():
+    wb.open_new('https://github.com/Parvat-web-dev/Simple-Cryptography/')
+    pass
 
 def encode(event = None):
     text = text_input.get('1.0', 'end-1c')
     Key = key.get()
     a = e.Cryptography(Key, text)
     b = a.encode(True)
-    text_return = Text(app)
-    text_return.config(fg = 'white', bg = 'black', font = 'Consolas 16')
-    text_return.insert('1.0', b)
-    text_return.update()
-    text_return.config(state = DISABLED)
-    text_return.config(height = 20, width = 27)
-    text_return.update()
-    text_return.place(x = 365, y = 10)
-    global code
-    code = b
+    text_return['text'] = b
 
 def decode(event = None):
     text = text_input.get('1.0', 'end-1c')
     Key = key.get()
     a = e.Cryptography(Key, text)
     b = a.decode(True)
-    text_return = Text(app)
-    text_return.config(fg = 'white', bg = 'black', font = 'Consolas 16')
-    text_return.insert('1.0', b)
-    text_return.update()
-    text_return.config(state = DISABLED)
-    text_return.config(height = 20, width = 27)
-    text_return.update()
-    text_return.place(x = 365, y = 10)
-    global code 
-    code = b
+    text_return['text'] = b
+    
 
-def Quit(event = None):
-    app.destroy()
+def NewWin(event = None):
+    import app
 
-def go_to():
-    try:
-        wb.open_new('https://github.com/Parvat-web-dev/Simple-Cryptography/')
-    except NameError:
-        print('The ~webdrive~ Module was not found!')
 def SaveAs(event = None):
     now = d.now()
     day = now.day
     month = now.month
     year = now.year
     time = now.time()
-    total = f'y/m/d : {year}/{month}/{day}, TIME: h/m/s/ms: {time}'
+    total = f'''y/m/d : {year}/{month}/{day}, TIME: h/m/s/ms: {time}
+'''
     extentions = [('Text File', '*.txt'), ('Document File', '*.doc')]
     FILE = f.asksaveasfile(mode = 'w', filetypes = extentions, defaultextension = extentions)
     KEY = key.get()
     TEXT = text_input.get('1.0', 'end-1c')
-    CODE = code
+    CODE = text_return['text']
     FILE.write(total)
     FILE.write(f'Key : {KEY}')
     FILE.write('''
 ''')
     FILE.write(f'Text : {TEXT}')
     FILE.write('''
-
 -------------------------------------------
-
 ''')
     FILE.write(f'Result : {CODE}')
     FILE.write('''
 ''')
 
-#The menu:
-menu = Menu(app)
+def Copy(event=None):
+    data = text_return['text']
+    Clipboard.copy(data)
 
-#The file menu:
+def Paste(event=None):
+    text_input.delete('1.0', 'end')
+    text_input.insert('end', Clipboard.paste())
+
 file = Menu(menu, tearoff = 0)
-file.add_cascade(label = 'Save As   Ctrl-s')
+file.add_cascade(label = 'New Window', command = NewWin)
+file.add_cascade(label = 'Save As', command = SaveAs)
 file.add_separator()
-file.add_cascade(label = 'Exit      Ctrl-q', command = app.destroy)
+file.add_cascade(label = 'Exit', command = app.destroy)
 
-#The cryptography menu:
 crypto = Menu(menu, tearoff = 0)
-crypto.add_cascade(label = ' Encode     Ctrl-e', command = encode)
-crypto.add_cascade(label = ' Decode     Ctrl-d', command = decode)
+crypto.add_cascade(label = 'Encode    Ctrl+e', command = encode)
+crypto.add_cascade(label = 'Decode    Ctrl+d', command = decode)
+crypto.add_cascade(label = 'Copy Encoded', command = Copy)
+crypto.add_cascade(label = 'Paste Text', command = Paste)
 
-#The help menu
-HeLp = Menu(menu, tearoff = 0)
-HeLp.add_cascade(label = ' Online Docs ', command = go_to)
-HeLp.add_cascade(label = '   Credits   ', command = Credits)
+def Credits():
+    cre = Tk()
+    cre.config(bg = 'black')
+    cre.geometry('570x400')
+    cre.title('Credits')
+    cre.resizable(False, False)
+    label = Label(cre, text = '''This GUI App is created by:
+        R. Parvat -
+Launched in github on :
+        Dec 31 2020   12:45
+This Program is created using:
+    1. Python 3.9   - Programming Language
+    2. Python IDLE      -      Text Editor
+    3. Tkinter       -       Python Module
+''', font = (('Curlz MT'),(24),('italic')), fg = 'white', bg = 'black')
+    label.place(x = 0, y = 0)
 
-#Adding all the menu in the main menu:
-menu.add_cascade(label = ' File ', menu = file)
-menu.add_cascade(label = ' Cryptography ', menu = crypto)
-menu.add_cascade(label = ' HELP ', menu = HeLp)
+Help = Menu(menu, tearoff = 0)
+Help.add_cascade(label = 'Online Docs')
+Help.add_cascade(label = '  Credits  ', command = Credits)
 
-#The shortcut keys:
+menu.add_cascade(label = 'File', menu = file)
+menu.add_cascade(label = 'Cryptography', menu = crypto)
+menu.add_cascade(label = 'Help', menu = Help)
+
 app.bind_all('<Control-Key-e>', encode)
 app.bind_all('<Control-Key-d>', decode)
-app.bind_all('<Control-Key-q>', Quit)
 app.bind_all('<Control-Key-s>', SaveAs)
+app.bind_all('<Control-Key-n>', NewWin)
+app.bind_all('<Control-Key-q>', quit)
 
-#The main loop of the window:
+text_input.bind_all('<Control-Key-e>', encode)
+text_input.bind_all('<Control-Key-d>', decode)
+text_input.bind_all('<Control-Key-s>', SaveAs)
+text_input.bind_all('<Control-Key-n>', NewWin)
+
+btn1 = Button(app, text = ' Encode ', command = encode, font = (('Consolas'),(18),('bold')), fg = 'white', bg = 'green')
+btn2 = Button(app, text = ' Decode ', command = decode, font = (('Consolas'),(18),('bold')), fg = 'darkgreen', bg = 'white')
+btn1.place(relx=0.6, y = 50, relwidth=0.2)
+btn2.place(relx=0.8, y = 50, relwidth=0.2)
+
+
 app.config(menu = menu)
 app.mainloop()
